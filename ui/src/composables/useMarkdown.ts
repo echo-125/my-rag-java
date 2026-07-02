1860e8c5ccfd0d07b27919d3f81945ef85a457bf
echo-125 <a1258477832@qq.com>
2026-07-02 08:21:28 +0800

    fix: 为 composables 补全缺失的 import { ref } from 'vue'
    useMarkdown 和 usePolling 缺少 ref 导入

--- ui/src/composables/useMarkdown.ts
+++ ui/src/composables/useMarkdown.ts
@@ -1,3 +1,4 @@
+import { ref } from 'vue'
 import { createHighlighter } from 'shiki'
 import markdownit from 'markdown-it'
 import { fromHighlighter } from '@shikijs/markdown-it'
--- ui/src/composables/usePolling.ts
+++ ui/src/composables/usePolling.ts
@@ -1,3 +1,4 @@
+import { ref } from 'vue'
 import { useIntervalFn } from '@vueuse/core'
 
 export function usePolling(
